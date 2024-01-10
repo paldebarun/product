61220e66e4475b87e93d260f1dccad25759b2839
Debarun <paldebarun27@gmail.com>
2024-01-10 20:20:03 +0530

    the contact form is made responsive

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -6,9 +6,12 @@ import { useLocation } from 'react-router-dom';
 const ContactForm = () => {
 
 
-    const { register, handleSubmit, formState: { errors } } = useForm();
+    const { register, handleSubmit, formState: { errors} , setError } = useForm();
 
     const onSubmit=(data)=>{
+
+       
+
         console.log("this is contact form data ",data);
         toast.success("the message is submitted");
      }
@@ -26,13 +29,13 @@ const ContactForm = () => {
          
 
           <form onSubmit={handleSubmit(onSubmit)} className=' h-[600px] p-5 w-full lg:w-6/12 gap-[20px] flex flex-col justify-center items-center'>
-                <div className='flex flex-col sm:flex-row gap-[20px]'>
+                <div className='flex flex-col w-full gap-[20px] sm:flex-row sm:justify-center items-center'>
                     
-                    <div className='flex flex-col gap-[5px] justify-center items-start'>
+                    <div className='flex flex-col  gap-[5px] justify-center items-start'>
                     <div className={inProductPage ?'text-white':'text-green-700'}>First Name</div>
                         <input 
                             type="text" 
-                            className='border rounded-md bg-slate-200 outline-none p-3 ' 
+                            className='border w-full sm:w-[200px] xl:w-[300px] rounded-md bg-slate-200 outline-none p-3 ' 
                             placeholder='First name' 
                             {...register('firstName', { required: true })}
                             
@@ -41,37 +44,39 @@ const ContactForm = () => {
                     </div>
                        
                    
-                    <div className='flex flex-col items-start gap-[5px]'>
+                    <div className='flex flex-col  items-start gap-[5px]'>
                        <div className={inProductPage ?'text-white':'text-green-700'}>Last Name</div>
                         <input 
                             type="text" 
-                            className='border rounded-md bg-slate-200 outline-none p-3' 
+                            className='border w-full sm:w-[200px] xl:w-[300px] rounded-md bg-slate-200 outline-none p-3' 
                             placeholder='Last name' 
                             {...register('lastName', { required: true })}
                         />
                         {errors.lastName && <span className='text-red-500 text-[10px]'>*This field is required</span>}
                     </div>
                 </div>
-                <div className='flex w-7/12 flex-col gap-[5px] items-start'>
+                <div className='flex  flex-col gap-[5px] items-start'>
                    <div className={inProductPage ?'text-white':'text-green-700'}>Email</div>
                     <input 
                         type="email" 
                         placeholder="Enter email" 
-                        className='border p-4 w-full bg-slate-200 rounded-md' 
+                        className='border p-4 w-full sm:w-[420px] xl:w-[620px] bg-slate-200 rounded-md' 
                         {...register('email', { required: true })}
                     />
                     {errors.email && <span className='text-red-500 text-[10px]'>*This field is required</span>}
                 </div>
-                <div className='flex flex-col w-7/12 items-start gap-[10px]'>
+                <div className='flex flex-col  items-start gap-[10px]'>
                    <div className={inProductPage ?'text-white':'text-green-700'}>Message</div>
                     <textarea 
                         placeholder="Message" 
-                        className='border outline-none p-5 w-full rounded-md bg-slate-200'
+                        className='border outline-none px-5 py-7 w-full sm:w-[420px] xl:w-[620px] rounded-md bg-slate-200'
                         {...register('message', { required: true })}
                     />
                     {errors.message && <span className='text-red-500 text-[10px]'>*This field is required</span>}
                 </div>
-                <div onClick={onSubmit} className='sliding-background hover:scale-110 transition-all duration-200 bg-green-700 text-white p-3 rounded-sm  hover:cursor-pointer'>Submit</div>
+                {/* <div onClick={onSubmit} className='sliding-background hover:scale-110 transition-all duration-200 bg-green-700 text-white p-3 rounded-sm  hover:cursor-pointer'>Submit</div> */}
+
+                <button type="submit" className='sliding-background hover:scale-110 transition-all duration-200 bg-green-700 text-white p-3 rounded-sm hover:cursor-pointer'>Submit</button>
             </form>
 
 
